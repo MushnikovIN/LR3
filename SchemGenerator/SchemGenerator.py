8a44c5a7e2a9fccedc7a624969540c9ed69fa387
MushnikovIN <ivanmusnikov53@gmail.com>
2026-04-08 12:42:57 +0300

    keep trying

--- SchemGenerator/SchemGenerator.py
+++ SchemGenerator/SchemGenerator.py
@@ -3,18 +3,21 @@ amplitude
 phase
 """
 
-
 class Branch:
-    def __init__(self, num: int, node_begin: int, node_end: int, type : str, G_eq: float = 0.0, J_eq: float = 0.0):
+    def __init__(self, num: int, node_begin: int, node_end: int, type : str, G: float = 0.0, J: float = 0.0):
         self.num = num
         self.node_begin = node_begin
         self.node_end = node_end
         self.R_eq = R_eq
         self.E_eq = E_eq
+
+
+
+
     
 class Circuit:
-    def __init__(self, branches: list[Branch] = None):
-        self.branches = branches if branches is not None else []
+    def __init__(self, circuit_data):
+        self.branches = SchemGenerator.scheme_gerate(circuit_data)
     
     def add_branch(self, branch: Branch):
         """Добавить ветвь в схему"""
@@ -23,12 +26,14 @@ class Circuit:
     def get_branch_count(self) -> int:
         """Получить количество ветвей в схеме"""
         return len(self.branches)
-    
-    def __repr__(self):
-        return f"Circuit(branches_count={len(self.branches)}, branches={self.branches})"
 
-"""
+
+
 class SchemGenerator:
-    def __init__(self, circuit_data : dict):
-        for i in dfdf
-"""
+    @staticmethod
+    def scheme_gerate(circuit_data : dict):
+        circuit_data = circuit_data.get('circuit', {})
+        branch_count = circuit_data.get('branch_count', {})
+        branches_data = circuit_data.get('branches', [])
+        for branch in branches_data:
+            #здесь создание объекта класса Branch и преобразование к JG-ветви
--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from Config.ConfigReader import read_config
 
 circuit_data = read_config('Config/config.json')
 
-circuit1  = Curcuit(circuit_data)
+#circuit1  = Curcuit(circuit_data)
+
+circuit_data1 = circuit_data.get('circuit', {})
+branches_data = circuit_data1.get('branches', [])
+print(circuit_data1)
+print(branches_data)
