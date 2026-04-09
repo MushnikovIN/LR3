73095914559589afe35ec540ab281ead54fcc33d
MushnikovIN <ivanmusnikov53@gmail.com>
2026-04-09 14:54:16 +0300

    version alpha

--- SchemGenerator/SchemGenerator.py
+++ SchemGenerator/SchemGenerator.py
@@ -2,22 +2,45 @@
 amplitude
 phase
 """
+import math
+import numpy as np
+
 
 class Branch:
-    def __init__(self, num: int, node_begin: int, node_end: int, type : str, G: float = 0.0, J: float = 0.0):
+    def __init__(self, num: int, node_begin: int, node_end: int, type : str, value : float,
+                 G: float = 0.0, J: float = 0.0, U : float = 0.0, i : float = 0.0, start_phase_deg : float = 0.0):
         self.num = num
         self.node_begin = node_begin
         self.node_end = node_end
-        self.R_eq = R_eq
-        self.E_eq = E_eq
+        self.type = type
+        self.value = value
+        self.G = G #Проводимость ветви в схеме замещения
+        self.J = J #Значение тока источника на некотором шаге
+        self.U = U #Значение напряжения на выводах ветви на некотором шаге
+        self.i = i #Значение тока ветви
+        self.start_phase_deg = start_phase_deg
+
+    def get_node_begin(self):
+        return self.node_begin
+
+    def get_node_end(self):
+        return self.node_end
+
+    def get_G(self):
+        return self.G
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
-
-
-    
 class Circuit:
-    def __init__(self, circuit_data):
-        self.branches = SchemGenerator.scheme_gerate(circuit_data)
+    def __init__(self, circuit_data, dt : float):
+        self.branches = SchemGenerator.scheme_gerate(circuit_data, dt)
+        self.branch_count = circuit_data.get('circuit', {}).get('branch_count', {})
+        self.node_count = circuit_data.get('circuit', {}).get('node_count', {})
+        #print(self.node_count)
+        self.A_matrix = SchemGenerator.make_A_matrix(self.branches, self.branch_count, self.node_count)
+        self.G_matrix, self.Gv_matrix= SchemGenerator.make_G_matrix(self.branches, self.A_matrix)
     
     def add_branch(self, branch: Branch):
         """Добавить ветвь в схему"""
@@ -27,13 +50,73 @@ class Circuit:
         """Получить количество ветвей в схеме"""
         return len(self.branches)
 
+    def get_all_branches(self):
+        return self.branches
+
+    def get_A_matrix(self):
+        return self.A_matrix
+
+    def get_G_matrix(self):
+        return self.G_matrix
+
+    def get_Gv_matrix(self):
+        return self.Gv_matrix
+
+    def __repr__(self):
+        return str(self.branches)
 
 
 class SchemGenerator:
     @staticmethod
-    def scheme_gerate(circuit_data : dict):
+    def scheme_gerate(circuit_data : dict, dt : float):
         circuit_data = circuit_data.get('circuit', {})
         branch_count = circuit_data.get('branch_count', {})
         branches_data = circuit_data.get('branches', [])
+        branches = {}
         for branch in branches_data:
+            start_phase = 0
+            match branch['type']:
+                case 'R':
+                    J = 0
+                    G = 1/branch['value']
+                case 'L':
+                    J = 0
+                    G = dt / 2 / branch['value']
+                case 'C':
+                    J = 0
+                    G = 2*branch['value']/dt
+                case 'E':
+                    J = branch['value']/0.000001
+                    G = 1/0.000001
+                case 'J':
+                    J = branch['value']
+                    G = 0
+                case 'Jsin':
+                    J = branch['value'][0]*math.sin(branch['value'][2]*math.pi/180)
+                    start_phase = branch['value'][2]
+                    G = 0
+
+            branches[branch['num']] = Branch(*branch.values(), G, J, 0, 0, start_phase)
+        return branches
             #здесь создание объекта класса Branch и преобразование к JG-ветви
+
+    @staticmethod
+    def make_A_matrix(branches : dict, branch_count : int, node_count : int):
+        A = np.zeros((node_count, branch_count))
+        for br in branches:
+            A[branches[br].get_node_begin(), br-1] = 1
+            A[branches[br].get_node_end(), br-1] = -1
+        A = np.delete(A, 0, axis=0) #Нулевой узел принимается за базисный всегда
+        #print(A)
+        return A
+
+    @staticmethod
+    def make_G_matrix(branches : dict, A : list):
+        Gv = []
+        for br in branches:
+            Gv.append(branches[br].get_G())
+        G = A @ np.diag(Gv) @ (A.T)
+        return G, Gv
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,61 @@
 from Config.ConfigReader import read_config
+from SchemGenerator.SchemGenerator import Circuit
+from Solver.Solver import Solver
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+
+filename = 'config3'
+circuit_data = read_config(f'Config/{filename}.json')
+
+dt = 0.000001
+T = 0.01
+
+circuit1  = Circuit(circuit_data, dt)
+#print(circuit1.get_all_branches())
+#print(circuit1)
+
+# circuit_data1 = circuit_data.get('circuit', {})
+# branches_data = circuit_data1.get('branches', [])
+#print(circuit_data1)
+#print(branches_data)
+
+U_res, I_res = Solver.solve_circuit(circuit1, dt, T)
+
+scipy.io.savemat(f'Modeling_results/{filename}_U.mat', {'U': U_res})
+scipy.io.savemat(f'Modeling_results/{filename}_I.mat', {'I': I_res})
+
+time_array = np.arange(0, T, dt)
 
 
-circuit_data = read_config('Config/config.json')
+# Создаем фигуру с подграфиками
+fig, axes = plt.subplots(2, 1, figsize=(12, 8))
 
-#circuit1  = Curcuit(circuit_data)
+time_ticks = np.arange(0, T+0.000001, 0.001)
 
-circuit_data1 = circuit_data.get('circuit', {})
-branches_data = circuit_data1.get('branches', [])
-print(circuit_data1)
-print(branches_data)
+# График токов
+ax1 = axes[0]
+label = f'Ток ветви 3'
+ax1.plot(time_array, I_res[1], label=label, linewidth=1.5)
+ax1.set_xlabel('Время, с')
+ax1.set_ylabel('Ток, А')
+ax1.set_title('Зависимость токов в ветвях от времени')
+ax1.grid(True, alpha=0.3)
+ax1.set_xticks(time_ticks)
+ax1.legend(loc='best')
+
+# График напряжений
+ax2 = axes[1]
+label = f'Напряжение узла 6'
+ax2.plot(time_array, U_res[6] , label=label, linewidth=1.5)
+ax2.set_xlabel('Время, с')
+ax2.set_ylabel('Напряжение, В')
+ax2.set_title('Зависимость напряжения узла от времени')
+ax2.grid(True, alpha=0.3)
+ax2.set_xticks(time_ticks)
+ax2.legend(loc='best')
+
+plt.tight_layout()
+plt.savefig(f'Modeling_results/{filename}_graph.png', dpi=150, bbox_inches='tight')
+print(f"\nГрафики сохранены в файл: {filename}_graph.png")
+plt.show()
